9ba212f96159623af050893b07cd80751739e9ae
Julius Ingeli <93543349+julius-ingeli@users.noreply.github.com>
2024-01-26 15:43:42 +0200

    fix

--- src/Julius_Ingeli_tasks/ex01.php
+++ src/Julius_Ingeli_tasks/ex01.php
@@ -7,12 +7,17 @@
 </head>
 <body>
     <?php
-        //T3.1:Write PHP code to output the following message:
+        echo"<h3>T3.1:Write PHP code to output the following message:</h3>";
         echo "<h3>Hello world! My name is \"David\"</h3>";
-        //T3.2:Create a PHP variable named $title and assign it the value "PHP is interesting." Then, use this variable as the content within an <h4> (heading 4) element.
-        $title = "PHP is interesting"; //I do not stand behind this statement.
+    ?>
+        <h3>T3.2:Create a PHP variable named $title and assign it the value "PHP is interesting. " Then, use this variable as the content within an h4 (heading 4) element.</h3>
+    <?php
+        $title = "\"PHP is interesting\""; //I do not stand behind this statement.
+
         echo "<h4>$title</h4>";
-        //T3.3:Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. These variables represent the grades of three students in the course. To display this information, create an HTML table within your PHP code. The table should be structured with columns for Serial Number (S.n.), Name, and Grade
+    ?>
+        <h3>T3.3:Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. These variables represent the grades of three students in the course. To display this information, create an HTML table within your PHP code. The table should be structured with columns for Serial Number (S.n.), Name, and Grade</h3>";
+    <?php
         $g1 = 5; $g2 = 4; $g3 = 5;
         echo"
         <table>
@@ -41,7 +46,7 @@
     </table>
     ";
     ?>
-    <!--T3.4 Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.-->
+    <h3>T3.4 Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.</h3>
     <img src="enviroment.png">
 
 </body>
